5853da22aae95b94d3e61c706cda332cd7b836a6
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2026-02-28 08:21:30 -0800

    Remove double website in tray popup

--- src/tray.rs
+++ src/tray.rs
@@ -32,7 +32,6 @@ impl AppTray {
                             version: Some(env!("CARGO_PKG_VERSION").to_owned()),
                             authors: Some(authors),
                             license: Some(env!("CARGO_PKG_LICENSE").to_owned()),
-                            website_label: Some(env!("CARGO_PKG_HOMEPAGE").to_owned()),
                             website: Some(env!("CARGO_PKG_HOMEPAGE").to_owned()),
                             comments: Some(r#"THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."#.to_owned()),
                             ..Default::default()
